b0309e97d11e515b33ae1afcb61d43c9142bbd9a
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-06-08 21:06:42 -0500

    feat(program): add configuration database

--- Corebyte-platform/Program.cs
+++ Corebyte-platform/Program.cs
@@ -1,13 +1,77 @@
+using Corebyte_platform.history_status.Application.Infernal.CommandServices;
+using Corebyte_platform.history_status.Application.Infernal.QueryServices;
+using Corebyte_platform.history_status.Domain.Repositories;
+using Corebyte_platform.history_status.Domain.Services;
+using Corebyte_platform.history_status.Infrastucture.Repositories;
+using Corebyte_platform.Shared.Domain.Repositories;
+using Corebyte_platform.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using Corebyte_platform.Shared.Infrastucture.Persistence.EFC.Configuration;
+using Corebyte_platform.Shared.Infrastucture.Persistence.EFC.Repositories;
+using EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure Lower Case URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
 
-var app = builder.Build();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString is null)
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+}
+
+// Configure Database Context and Logging Levels
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>
+        {
+            options.UseMySql(connectionString, serverVersion)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        });
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>
+        {
+            options.UseMySql(connectionString, serverVersion)
+                .LogTo(Console.WriteLine, LogLevel.Error)
+                .EnableDetailedErrors();
+        });
+
+// Configure Dependency Injection
+
+// Shared Bounded Context Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+// News Bounded Context Injection Configuration
+builder.Services.AddScoped<IHistoryRepository, HistoryRepository>();
+builder.Services.AddScoped<IHistoryCommandService, HistoryCommandService>();
+builder.Services.AddScoped<IHistoryQueryService, HistoryQueryService>();
+
+var app= builder.Build();
+
+
+// Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
