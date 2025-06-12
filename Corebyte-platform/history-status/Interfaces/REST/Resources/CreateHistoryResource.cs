e3be8a190af7022a16f9e34247b31b66fc78d552
equinox-1092 <sdiaz1234di@gmail.com>
2025-06-12 11:35:58 -0500

    feat(history-status): add documentation and configuration controller history and record

---
+++ Corebyte-platform/history-status/Interfaces/REST/Resources/CreateHistoryResource.cs
@@ -0,0 +1,13 @@
+﻿namespace Corebyte_platform.history_status.Interfaces.REST.Resources
+{
+    /// <summary>
+    /// Represents the data required to create a history. 
+    /// </summary>
+    /// <param name="customer">The customer</param>
+    /// <param name="date">The date</param>
+    /// <param name="product">The product</param>
+    /// <param name="amount">The amount</param>
+    /// <param name="total">The total</param>
+    /// <param name="status">The status</param>
+    public record CreateHistoryResource(string customer, DateTime date, string product, int amount, double total, string status);
+}
