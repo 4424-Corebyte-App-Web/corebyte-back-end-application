97bbe337223dc62f99052c5cac13fc8b0ff7c390
Giovany Torres <u202311601@upc.edu.pe>
2025-06-19 20:21:48 -0500

    feat(create): Create order command

---
+++ Corebyte-platform/orders/Domain/Model/Commands/CreateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Corebyte_platform.orders.Domain.Model.Commands
+{
+    /// <summary>
+    /// command to create a order entry.
+    /// </summary>
+    /// <param name="customer">The customer obtained from provider</param>
+    /// <param name="date">The date of the order</param>
+    /// <param name="product">The product of the order</param>
+    /// <param name="amount">The amount of the order</param>
+    /// <param name="total">The total of the order</param>
+    /// 
+    public record CreateOrderCommand(string customer, DateTime date, string product, int amount, double total);
+   
+}
